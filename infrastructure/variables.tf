25e1489ff3ca54ce0f271fd102d872cd42611a74
Talambert <t.alambert@gmail.com>
2021-08-17 16:38:49 -0300

    add infrastructure

---
+++ infrastructure/variables.tf
@@ -0,0 +1,19 @@
+variable "base_bucket_name" {
+  default = "datalake-igti-tf"
+}
+
+variable "ambiente" {
+  default = "producao"
+}
+
+variable "numero_conta" {
+  default = "421168935276"
+}
+
+variable "aws_region" {
+  default = "us-east-2"
+}
+
+
+
+
